b133ca6c5a6c94758ff59d44e5851c70fd5731df
Ingehau <ih93@hotmail.com>
2025-01-17 21:04:58 +0100

    Update Arbeidskrav_2.py

--- Arbeidskrav_2.py
+++ Arbeidskrav_2.py
@@ -106,6 +106,7 @@ print("Figurens omkrets er:", omkrets)
 
 # Oppgave 6
 # Kode som plotter funksjonen f(x) = -x^2 - 5 for x i intervallet [-10, 10].
+import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(-10, 10, 200)
